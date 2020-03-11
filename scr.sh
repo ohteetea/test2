5b1a162c52380f4d788f2c93d790634c103b04bd
Becca <rebecca.culp@ustglobal.com>
2020-03-11 14:10:01 -0700

    more lines added

--- scr.sh
+++ scr.sh
@@ -2,7 +2,16 @@
 
 echo this line right here
 
-ls -1
+ls -l
 cat script.sh
 cd /c/Temp
 #This is a comment
+echo $1 # numbers are command line args. This is arg #1
+echo $2 # This is arg #2.
+echo
+read -p "Enter a number: " i
+read -p "Enter another number: " j
+echo "The sum of the number is " $(( $i + $j ))
+cp /c/eclipse-workspace/ch01_FutureValueConsole/src/murach/fv/Main.java ./
+cd "/c/Program Files/Java/jdk1.8.0_181/bin"
+javac Main.java
